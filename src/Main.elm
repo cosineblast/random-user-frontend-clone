beb9ac609b3e1b9f138e431904cb97dfc7f58e96
cosineblast <55855728+cosineblast@users.noreply.github.com>
2024-06-29 12:20:10 -0300

    Initial Commit

---
+++ src/Main.elm
@@ -0,0 +1,44 @@
+
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Browser
+
+main =
+    div []
+    [ header [class "flex justify-center pt-10 bg-zinc-800 text-white pb-10"]
+        [ div [class "flex flex-col items-center text-center"]
+            [ p [class "text-5xl mb-5"] [text "RANDOM USER GENERATOR"]
+            , p [class "mb-2"] [ text "A free, open-source API for generating random user data. Like Lorem Ipsum, but for people." ]
+            , p [class "text-zinc-400"] [ text "Follow us @randomapi"]
+            ]
+
+        ]
+    , main_ [class "max-w-screen-md mx-auto pt-20"]
+        [
+            section [class "rounded-lg shadow-lg p-2 relative mx-5"]
+            [ figure [class "absolute left-0 right-0 top-10 flex justify-center"]
+                [ div [class "border-solid border-2 border-zinc-300 rounded-full p-1 bg-white"]
+                    [ img [class "rounded-full w-40 h-40", src "https://i.redd.it/ljfpcj6bdih41.jpg"] []
+                    ]
+                ]
+            , div [class "pt-20 pb-10"] []
+            , hr [] []
+            , div [class "pt-28 flex flex-col items-center gap-2 pb-5"]
+                [ p [class "text-zinc-500"] [ text "My name is" ]
+                , p [class "text-5xl"] [ text "God" ]
+                , div [class "flex flex-row gap-4 flex-wrap justify-center"]
+                    [ p [] [ text "Name"]
+                    , p [] [ text "E-mail"]
+                    , p [] [ text "Birthday"]
+                    , p [] [ text "Address"]
+                    , p [] [ text "Phone number"]
+                    , p [] [ text "Password"]
+                    ]
+
+
+                ]
+            ]
+        ]
+    ]
